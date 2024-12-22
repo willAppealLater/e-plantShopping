706dc6499a3d4ed9348c0e70f11647f20598eddb
Moeed Habib <lolmoeed421@gmail.com>
2024-12-22 07:06:47 -0500

    second test

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -38,4 +38,4 @@ export const CartSlice = createSlice({
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
 
-export default CartSlice.reducer,;
+export default CartSlice.reducer;
